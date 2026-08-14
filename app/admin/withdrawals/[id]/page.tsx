adeaa763b71e51c71065e00175b3c1ae97eb5eb1
unknown <hulogmoutangko@gmail.com>
2026-08-14 14:33:41 +0800

    Fix: Withdrawal Page

---
+++ app/admin/withdrawals/[id]/page.tsx
@@ -0,0 +1,11 @@
+import CoopAccountWithdrawal from "../withdrawal-form";
+
+export default async function WithdrawalPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+
+  return (
+    <div style={{ maxWidth: 700, margin: "40px auto", padding: "0 20px" }}>
+      <CoopAccountWithdrawal coopAccountId={id} />
+    </div>
+  );
+}
